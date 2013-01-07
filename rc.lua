eb628e01e95c707bb7b22b3dccf52aa1f8fa952a
Jan Hruban <jan.hruban@sde.cz>
2013-01-07 18:28:43 +0100

    move clients between screens with mod

--- rc.lua
+++ rc.lua
@@ -301,10 +301,10 @@ globalkeys = awful.util.table.join(
 clientkeys = awful.util.table.join(
     awful.key({ modkey, "Shift"   }, "f",      function (c) c.fullscreen = not c.fullscreen  end),
     awful.key({ modkey,           }, "Escape", function (c) c:kill()                         end),
-    awful.key({ modkey,           }, "f",  awful.client.floating.toggle                     ),
+    awful.key({ modkey,           }, "f",      awful.client.floating.toggle                     ),
     awful.key({ modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end),
-    awful.key({ modkey, "Control" }, ",",      awful.client.movetoscreen                        ),
-    awful.key({ modkey, "Control" }, ".",      awful.client.movetoscreen                        ),
+    awful.key({ modkey,           }, ",",      awful.client.movetoscreen                        ),
+    awful.key({ modkey,           }, ".",      awful.client.movetoscreen                        ),
     awful.key({ modkey,           }, "t",      function (c) c.ontop = not c.ontop            end),
 
     awful.key({ modkey,           }, "n",
