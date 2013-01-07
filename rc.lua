3c6a96ba42c151589f0658926635aac5127800c3
Jan Hruban <hrubi@hrubi.cz>
2013-01-07 21:20:23 +0100

    resize/move clients with Alt

--- rc.lua
+++ rc.lua
@@ -353,8 +353,8 @@ end
 
 clientbuttons = awful.util.table.join(
     awful.button({ }, 1, function (c) client.focus = c; c:raise() end),
-    awful.button({ modkey }, 1, awful.mouse.client.move),
-    awful.button({ modkey }, 3, awful.mouse.client.resize))
+    awful.button({ altkey }, 1, awful.mouse.client.move),
+    awful.button({ altkey }, 3, awful.mouse.client.resize))
 
 -- Set keys
 root.keys(globalkeys)
